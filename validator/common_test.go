7bed2b0f841cc35fc1284411548bdba5fa1b91dc
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2024-03-13 08:40:14 +0700

    feat: add custom validator

--- validator/common_test.go
+++ validator/common_test.go
@@ -17,3 +17,21 @@ func TestPointerNotNil(t *testing.T) {
 		require.ErrorContains(st, PointerNotNil[testdata.User]("user", nil)(), "must not be nil")
 	})
 }
+
+func TestCustom(t *testing.T) {
+	t.Run("OK", func(st *testing.T) {
+		require.NoError(st, Custom("custom", &custom{})())
+	})
+
+	t.Run("OK", func(st *testing.T) {
+		require.Error(st, Custom("custom", &custom{err: testdata.ErrGeneric})())
+	})
+}
+
+type custom struct {
+	err error
+}
+
+func (v *custom) Validate() error {
+	return v.err
+}
