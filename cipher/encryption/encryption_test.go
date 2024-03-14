d2cd0c15f61d8a11f82cb23ab59b62b3bdb85da0
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2024-03-14 09:11:11 +0700

    feat: add encryption package

---
+++ cipher/encryption/encryption_test.go
@@ -0,0 +1,64 @@
+package encryption
+
+import (
+	"crypto/aes"
+	"encoding/base64"
+	"strings"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/jaswdr/faker"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncryption(t *testing.T) {
+	key := genkey(32)
+	data := faker.New().Lorem().Sentence(256)
+
+	ciphertext, err := Encrypt(key, data)
+	assert.Nil(t, err)
+
+	original, err := Decrypt(key, ciphertext)
+	assert.Nil(t, err)
+
+	assert.Equal(t, data, original)
+}
+
+func TestEncryption_Encrypt(t *testing.T) {
+	t.Run("KO - empty data", func(t *testing.T) {
+		key := genkey(32)
+		s, err := Encrypt(key, "")
+		assert.NoError(t, err)
+		assert.NotEmpty(t, s)
+	})
+
+	t.Run("KO - empty key error", func(t *testing.T) {
+		data := faker.New().Lorem().Sentence(256)
+		_, err := Encrypt("", data)
+		assert.Error(t, err)
+	})
+}
+
+func TestEncryption_Decrypt(t *testing.T) {
+	t.Run("KO - encrypted base64 decode error", func(t *testing.T) {
+		encrypted := faker.New().Lorem().Sentence(256)
+		_, err := Decrypt("", encrypted)
+		assert.Error(t, err)
+	})
+
+	t.Run("KO - encrypted text size error", func(t *testing.T) {
+		encrypted := base64.StdEncoding.EncodeToString([]byte(genkey(aes.BlockSize - 1)))
+		_, err := Decrypt(genkey(32), encrypted)
+		assert.Error(t, err)
+	})
+}
+
+func genkey(n int) string {
+	var str string
+	count := n / 32
+	for i := 0; i <= count; i++ {
+		str += strings.ReplaceAll(uuid.NewString(), "-", "")
+	}
+
+	return str[:n]
+}
