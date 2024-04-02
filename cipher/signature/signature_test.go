c34ea2703fd9e1c65eb72c4a9b4483735fcd2605
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2024-04-02 20:42:27 +0700

    test: add signature test with chinese char

--- cipher/signature/signature_test.go
+++ cipher/signature/signature_test.go
@@ -2,10 +2,12 @@ package signature
 
 import (
 	"fmt"
+	"testing"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/kanthorlabs/common/testdata"
+	"github.com/stretchr/testify/require"
 )
 
 var key = uuid.NewString()
@@ -15,3 +17,21 @@ var data = fmt.Sprintf(
 	testdata.Fake.Lorem().Sentence(1),
 	time.Now().UTC().UnixMilli(),
 )
+
+func TestSignature(t *testing.T) {
+	t.Run("English", func(st *testing.T) {
+		engkey := uuid.NewString()
+		engdata := "The quick brown fox jumps over the lazy dog"
+
+		sign := Sign(engkey, engdata)
+		require.NoError(st, Verify(engkey, engdata, sign))
+	})
+
+	t.Run("Chinese", func(st *testing.T) {
+		chinesekey := "相应中文可简译为“快狐跨懒狗”，完整翻译则是“敏捷的棕色狐狸跨过懒狗"
+		chinesedata := "相应中文可简译为“快狐跨懒狗”，完整翻译则是“敏捷的棕色狐狸跨过懒狗"
+
+		sign := Sign(chinesekey, chinesedata)
+		require.NoError(st, Verify(chinesekey, chinesedata, sign))
+	})
+}
